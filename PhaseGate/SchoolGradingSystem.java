d643636466ab2ce57152bd770582ba5054ba9912
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-12-03 11:07:26 +0100

    First commit

---
+++ PhaseGate/SchoolGradingSystem.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class SchoolGradingSystem{
+	public static void main(String[] args){
+	
+	Scanner input = new Scanner(System.in);
+   
+
+	ArrayList<Integer> Studentamount = new ArrayList<Integer>();
+	ArrayList<Integer> Subject = new ArrayList<Integer>();	
+	ArrayList<Integer> StudentScore = new ArrayList<Integer>();
+
+		System.out.println("How many students do you have ?");
+		int numberOfStudent = input.nextInt();
+		
+		System.out.println("How many subjects do they offer ?");
+		int numberOfSubject = input.nextInt();
+		
+		System.out.println("Saving>>>>>>>>>>>>>>>>>");
+		
+		System.out.println("Saved Successfully ");
+		 
+
+
+
+
+
+
+
+
+
+
+
+}
+}
