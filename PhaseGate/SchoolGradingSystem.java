fcbd3ad4345152a575e148e0d27d75f56c8f9baa
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-12-04 08:10:05 +0100

    pus this thing oo

--- PhaseGate/SchoolGradingSystem.java
+++ PhaseGate/SchoolGradingSystem.java
@@ -7,10 +7,6 @@ public class SchoolGradingSystem{
 	
 	Scanner input = new Scanner(System.in);
    
-
-	int Studentamount [] = new int[0];
-	int Subject[] = new int[0];	
-	
 		System.out.println("How many students do you have ?");
 		int numberOfStudent = input.nextInt();
 		
@@ -19,28 +15,45 @@ public class SchoolGradingSystem{
 
 		System.out.println("Saving>>>>>>>>>>>>>>>>>");
 		System.out.println("Saved Successfully ");
+
+		int [] [] student = new int [numberOfStudent][numberOfSubject];
+		int [] total = new int[numberOfStudent];
+		int [] newTotal = new int [numberOfStudent];
+		double [] average = new double [numberOfStudent];
+		int [] position = new int [numberOfStudent];
+
 		
-		int studentcounter = 0;
-			System.out.println("Entering score for student 1");
-			int StudentScore[] = new int[numberOfStudent];
+		for(int index = 0,counter = 1; index < numberOfStudent; index++,counter++){
+			int sum = 0;
+			for(int element = 0, count = 1; element < numberOfSubject; element++,count++){
+				System.out.println("Entering score for student"+ counter +"\nEnter score for subject"+count+ " : ");
+				student[index][element] = input.nextInt();
+				System.out.println("Saving>>>>>>>>>>>>>");
+				System.out.println("Saved Successfully");
+				System.out.println();
 
-			while(studentcounter != numberOfStudent){
-
-				System.out.print("Entering Score for subject 1 :");
-					int studentscore = input.nextInt();	
-					for(int i = 0; i < studentscore; i++){
-						StudentScore.push(i);						
-						}
-					studentcounter++;
-						}
-						System.out.println("Saving>>>>>>>>>>>>>");
-						System.out.println("Saved Successfully");
-
-						for(int count = 0; count < StudentScore.length ; count ++){
-								System.out.print(count);
-								}
+				sum+= student[index][element];
 
 
+				System.out.println();
+				}
+				
+				total[index] = sum;
+				newTotal[index]= sum;
+				average[index] = sum / numberOfSubject;
+				}
+
+			Arrays.sort(newTotal);
+
+			for(int index = 0, count =1; index < student.length; index++,element++){
+ 					
+				for(int element = 0; element < student.length; element++){
+				if(total[index]== newTotal[element]){
+					position[index] = student.length - element;
+
+			}
+			}
+			}
 
 
 
