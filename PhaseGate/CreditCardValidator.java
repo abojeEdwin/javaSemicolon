060f47de21f6f09ac096e45443eb49f19a049ccc
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-12-05 07:12:28 +0100

    second credit card commit

--- PhaseGate/CreditCardValidator.java
+++ PhaseGate/CreditCardValidator.java
@@ -16,21 +16,19 @@ public class CreditCardValidator{
 
 			if(numbers.length >=13 && numbers.length<=16){
 				checkPrefix(numbers);
-				displayCreditCardNumber(numbers);
-				displayCreditCardLength(numbers);
-				determineCardValidity(numbers);
 				}else{
 					System.out.println("Invalid card numbers");
-					System.out.println("The length must be between 13 and 16")
+					System.out.println("The length must be between 13 and 16");
 }
-		public static String [] checkPrefix(String[] numbers){
+}
+			public static String [] checkPrefix(String[] numbers){
 			
-				if(nmbers[0].equals("4")){
+				if(numbers[0].equals("4")){
 					System.out.println("Credit Card Type : VisaCard");
 				}else if (numbers[0].equals("5")){
 						System.out.println("Credit Card Type : MasterCard");
 				} else if (numbers[0].equals("3")){
-						System.our.println("CreditCard Type : American Express Card");
+						System.out.println("CreditCard Type : American Express Card");
 				} else if (numbers[0].equals("6")){
 						System.out.print("CreditCard Type : Discover Card");
 				} else{
