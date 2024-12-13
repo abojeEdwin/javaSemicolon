77b2868a24d75c64afa61a3e2e96340afc435d13
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-12-13 01:21:17 +0100

    cipher cryptography update

--- assignments/TestCeaserCryptography.java
+++ assignments/TestCeaserCryptography.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCeaserCryptography{
-	
+	@Test
 	public void CipherEncryptedMsgReturunsCorrectValue(){
 	//start
 	CeaserCryptography cipherEncrypted = new CeaserCryptography();
